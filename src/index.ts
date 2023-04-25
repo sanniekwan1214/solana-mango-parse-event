19d728ce3602b4fbb17a9db09480e0a2fb6cf87c
sanniekwan1214 <sanniekwan1214@gmail.com>
2023-04-24 18:21:18 -0700

    feat: support poll and env config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { solanaProvider } from './solanaProvider'
-function main() {
-    solanaProvider()
-}
+import dotenv from 'dotenv';
+import { Connection, PublicKey } from '@solana/web3.js';
 
-main();
+dotenv.config();
+const rpcUrl = process.env.RPC_URL;
+const connection = new Connection(rpcUrl);
+
+// Get the public key for the program account
+const programId = new PublicKey(process.env.MANGO_ACC);
+
+// Poll every 20 seconds due to rate limit
+setInterval(async () => {
+    solanaProvider(connection, programId);
+}, parseInt(process.env.POLL_FREQUENCY))
